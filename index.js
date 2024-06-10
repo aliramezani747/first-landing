eb33413d8edbf31490223641c1fc955b02ebd0b2
aliramezani747 <epaliramezani747@gmail.com>
2024-06-10 21:34:11 +0330

    add description card

--- index.js
+++ index.js
@@ -32,3 +32,16 @@ function showButton() {
     button.style.display = "none";
   }
 }
+
+function showDescription(product) {
+  let cards = document.getElementsByClassName("cards");
+  for (var i = 0; i < cards.length; i++) {
+    let card = cards[i];
+    let title = card.getElementsByTagName("h2")[0].textContent;
+    let description = card.getElementsByClassName("description")[0].textContent;
+    if (title === product) {
+      document.getElementById("descriptionDisplay").textContent = description;
+      document.getElementById("descriptionDisplay").style.display = "block";
+    }
+  }
+}
